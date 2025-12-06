beca1ed552b83889e1c8ce961491d798f54f7bb5
Eric Tu <ceparadise168@gmail.com>
2025-12-07 01:59:59 +0800

    feat: 啟用 Terraform S3 遠端後端用於狀態共享

--- terraform/main.tf
+++ terraform/main.tf
@@ -8,18 +8,14 @@ terraform {
     }
   }
 
-  # Best practice: Use a remote backend like S3 to store the state file
-  # This allows for team collaboration and keeps state off local machines.
-  # We will configure this part in the CI/CD pipeline.
-  /*
+  # Remote backend for shared Terraform state
   backend "s3" {
-    bucket         = "echo-tree-tfstate-bucket" # Needs to be created manually and be unique
+    bucket         = "echo-tree-tfstate-470099689432"
     key            = "global/s3/terraform.tfstate"
     region         = "us-east-1"
     encrypt        = true
     dynamodb_table = "terraform-lock"
   }
-  */
 }
 
 provider "aws" {
