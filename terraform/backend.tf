ce91804b5404c783dc5898b81a0653b354b4f384
Eric Tu <ceparadise168@gmail.com>
2025-12-07 02:28:13 +0800

    fix: 改用 AWS_PROXY 整合處理 OPTIONS 以解決 CORS 500 錯誤

--- terraform/backend.tf
+++ terraform/backend.tf
@@ -147,43 +147,14 @@ resource "aws_api_gateway_method" "options_method" {
   authorization = "NONE"
 }
 
-resource "aws_api_gateway_method_response" "options_response" {
-  rest_api_id = aws_api_gateway_rest_api.api.id
-  resource_id = aws_api_gateway_resource.cards_resource.id
-  http_method = aws_api_gateway_method.options_method.http_method
-  status_code = "200"
-  response_models = {
-    "application/json" = "Empty"
-  }
-  response_parameters = {
-    "method.response.header.Access-Control-Allow-Headers" = true,
-    "method.response.header.Access-Control-Allow-Methods" = true,
-    "method.response.header.Access-Control-Allow-Origin"  = true
-  }
-}
-
+# OPTIONS integration - proxy to Lambda for consistent CORS handling
 resource "aws_api_gateway_integration" "options_integration" {
-  rest_api_id = aws_api_gateway_rest_api.api.id
-  resource_id = aws_api_gateway_resource.cards_resource.id
-  http_method = aws_api_gateway_method.options_method.http_method
-  type        = "MOCK"
-  request_templates = {
-    "application/json" = "{\"statusCode\": 200}"
-  }
-}
-
-resource "aws_api_gateway_integration_response" "options_integration_response" {
-  rest_api_id = aws_api_gateway_rest_api.api.id
-  resource_id = aws_api_gateway_resource.cards_resource.id
-  http_method = aws_api_gateway_method.options_method.http_method
-  status_code = aws_api_gateway_method_response.options_response.status_code
-  response_parameters = {
-    "method.response.header.Access-Control-Allow-Headers" = "'Content-Type,X-Amz-Date,Authorization,X-Api-Key,X-Amz-Security-Token'",
-    "method.response.header.Access-Control-Allow-Methods" = "'GET,POST,OPTIONS'",
-    "method.response.header.Access-Control-Allow-Origin"  = "'*'"
-  }
-  
-  depends_on = [aws_api_gateway_integration.options_integration]
+  rest_api_id             = aws_api_gateway_rest_api.api.id
+  resource_id             = aws_api_gateway_resource.cards_resource.id
+  http_method             = aws_api_gateway_method.options_method.http_method
+  integration_http_method = "POST"
+  type                    = "AWS_PROXY"
+  uri                     = aws_lambda_function.cards_api_lambda.invoke_arn
 }
 
 # Deployment of the API Gateway
@@ -199,9 +170,7 @@ resource "aws_api_gateway_deployment" "api_deployment" {
       aws_api_gateway_method.post_cards_method.id,
       aws_api_gateway_integration.post_cards_integration.id,
       aws_api_gateway_method.options_method.id,
-      aws_api_gateway_integration.options_integration.id,
-      aws_api_gateway_method_response.options_response.id,
-      aws_api_gateway_integration_response.options_integration_response.id
+      aws_api_gateway_integration.options_integration.id
     ]))
   }
 
